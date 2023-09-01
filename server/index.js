09f83ff609905e23ae0bc0b24b82296ecbb7eede
Sudhanshu Pal <palsudhanshu177@gmail.com>
2023-09-01 11:55:16 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use('/', Router);
 //     })
 // })
 
-const PORT = "https://blogging-hub.onrender.com";
+const PORT = process.env.PORT || 8000;
 const username = process.env.DB_USERNAME; 
 const password = process.env.DB_PASSWORD;
 
